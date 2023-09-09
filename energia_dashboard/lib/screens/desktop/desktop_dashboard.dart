5481799b2e94f343d9db94057fb8f92ab3a03315
DevPrinceK <80714449+DevPrinceK@users.noreply.github.com>
2023-09-09 12:55:16 +0000

    Update desktop_dashboard.dart

--- energia_dashboard/lib/screens/desktop/desktop_dashboard.dart
+++ energia_dashboard/lib/screens/desktop/desktop_dashboard.dart
@@ -5,6 +5,41 @@ class DesktopDashboard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Icon(Icons.menu),
+        title: const Text(
+          'Energia AI',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+        actions: [
+          Container(
+            margin: const EdgeInsets.only(right: 10),
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 8),
+            height: 50,
+            decoration: const BoxDecoration(
+              borderRadius: BorderRadius.all(
+                Radius.circular(15),
+              ),
+              color: Colors.black,
+            ),
+            child: const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Text(
+                  "princesamuel@gmail.com",
+                  style: TextStyle(color: Colors.white),
+                ),
+                SizedBox(width: 5),
+                CircleAvatar(
+                  child: Icon(Icons.person),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
