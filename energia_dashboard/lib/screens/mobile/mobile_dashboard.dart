ee0d8b35160ba009aa4e2245ed24d01567f10012
ofori_Prince <Pofori034@st.ug.edu.gh>
2023-09-09 17:41:39 +0000

    update on mobile app

--- energia_dashboard/lib/screens/mobile/mobile_dashboard.dart
+++ energia_dashboard/lib/screens/mobile/mobile_dashboard.dart
@@ -1,10 +1,80 @@
-import 'package:flutter/material.dart';
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors, library_private_types_in_public_api, use_key_in_widget_constructors
 
-class MobileDashboard extends StatelessWidget {
-  const MobileDashboard({super.key});
+import 'package:energia_dashboard/screens/mobile/tabs/analysis_tab.dart';
+import 'package:energia_dashboard/screens/mobile/tabs/dashboard_tab.dart';
+import 'package:energia_dashboard/screens/mobile/tabs/forcast_tab.dart';
+import 'package:energia_dashboard/screens/mobile/tabs/prediction_tab.dart';
+import 'package:energia_dashboard/screens/mobile/tabs/profile_tab.dart';
+import 'package:flutter/material.dart';
+import 'package:convex_bottom_bar/convex_bottom_bar.dart';
+
+class MobileDashboard extends StatefulWidget {
+  const MobileDashboard({Key? key}) : super(key: key);
+
+  @override
+  _MobileDashboardState createState() => _MobileDashboardState();
+}
+
+class _MobileDashboardState extends State<MobileDashboard> {
+  int _currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        leading: Padding(
+          padding: const EdgeInsets.only(left: 10.0),
+          child: Image.asset(
+            'assets/images/logo.jpg',
+            width: 40,
+          ),
+        ),
+        title: const Text(
+          'Energia AI',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 10.0),
+            child: CircleAvatar(
+              child: Icon(Icons.person),
+            ),
+          )
+        ],
+        centerTitle: true,
+      ),
+      body: _buildScreen(_currentIndex),
+      bottomNavigationBar: ConvexAppBar(
+        items: [
+          TabItem(icon: Icons.home, title: 'Dashboard'),
+          TabItem(icon: Icons.location_on, title: 'Prediction'),
+          TabItem(icon: Icons.analytics, title: 'Analysis'),
+          TabItem(icon: Icons.cloud, title: 'Forecast'),
+          TabItem(icon: Icons.people, title: 'Profile'),
+        ],
+        onTap: (int index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+      ),
+    );
+  }
+
+  Widget _buildScreen(int index) {
+    switch (index) {
+      case 0:
+        return DashboardScreen();
+      case 1:
+        return PredictionScreen();
+      case 2:
+        return AnalysisScreen();
+      case 3:
+        return ForecastScreen();
+      case 4:
+        return ProfileScreen();
+      default:
+        return Container();
+    }
   }
 }
---
+++ energia_dashboard/lib/screens/mobile/tabs/analysis_tab.dart
@@ -0,0 +1,18 @@
+// ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors
+
+import 'package:flutter/material.dart';
+
+class AnalysisScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: Center(
+        child: Text(
+          'Analysis',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
