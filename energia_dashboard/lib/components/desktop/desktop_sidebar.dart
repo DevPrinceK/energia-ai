69641793776467e4a83fee0f1bfee37b9e97507b
DevPrinceK <80714449+DevPrinceK@users.noreply.github.com>
2023-09-10 09:56:51 +0000

    Update desktop_sidebar.dart

--- energia_dashboard/lib/components/desktop/desktop_sidebar.dart
+++ energia_dashboard/lib/components/desktop/desktop_sidebar.dart
@@ -1,7 +1,21 @@
+import 'package:energia_dashboard/screens/desktop/desktop_analyze.dart';
+import 'package:energia_dashboard/screens/desktop/desktop_dashboard.dart';
+import 'package:energia_dashboard/screens/desktop/desktop_forecast.dart';
+import 'package:energia_dashboard/screens/desktop/desktop_predict.dart';
 import 'package:flutter/material.dart';
 
 class DesktopSidebar extends StatelessWidget {
-  const DesktopSidebar({super.key});
+  final bool dashboardActive;
+  final bool analyzeActive;
+  final bool predictActive;
+  final bool forecastActive;
+  const DesktopSidebar({
+    super.key,
+    this.dashboardActive = false,
+    this.analyzeActive = false,
+    this.predictActive = false,
+    this.forecastActive = false,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -16,35 +30,74 @@ class DesktopSidebar extends StatelessWidget {
         children: [
           const SizedBox(height: 30),
           ListTile(
-            selectedColor: Colors.white,
+            selectedTileColor: Colors.black87,
             leading: const Icon(Icons.dashboard),
             title: const Text("Dashboard"),
-            onTap: () {},
+            selected: dashboardActive,
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) {
+                    return const DesktopDashboard();
+                  },
+                ),
+              );
+            },
           ),
-          const InkWell(
-            child: ListTile(
-              leading: Icon(Icons.analytics),
-              title: Text("Analysis"),
-            ),
+          ListTile(
+            selectedTileColor: Colors.black87,
+            leading: const Icon(Icons.analytics),
+            title: const Text("Analysis"),
+            selected: analyzeActive,
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) {
+                    return const DesktopAnalyze();
+                  },
+                ),
+              );
+            },
           ),
-          const InkWell(
-            child: ListTile(
-              leading: Icon(Icons.lan_sharp),
-              title: Text("Prediction"),
-            ),
+          ListTile(
+            selectedTileColor: Colors.black87,
+            leading: const Icon(Icons.lan_sharp),
+            title: const Text("Prediction"),
+            selected: predictActive,
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) {
+                    return const DesktopPredict();
+                  },
+                ),
+              );
+            },
           ),
-          const InkWell(
-            child: ListTile(
-              leading: Icon(Icons.timeline_outlined),
-              title: Text("Forecast"),
-            ),
+          ListTile(
+            selectedTileColor: Colors.black87,
+            leading: const Icon(Icons.timeline_outlined),
+            title: const Text("Forecast"),
+            selected: forecastActive,
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) {
+                    return const DesktopForecast();
+                  },
+                ),
+              );
+            },
           ),
           const Spacer(),
-          const InkWell(
-            child: ListTile(
-              leading: Icon(Icons.power_settings_new_outlined),
-              title: Text("Sign Out"),
-            ),
+          const ListTile(
+            selectedTileColor: Colors.red,
+            leading: Icon(Icons.power_settings_new_outlined),
+            title: Text("Sign Out"),
           ),
         ],
       ),
