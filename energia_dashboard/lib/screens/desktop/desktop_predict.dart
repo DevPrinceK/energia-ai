525f98a08031915f938b59a0ff4bb9563cb3c33e
DevPrinceK <80714449+DevPrinceK@users.noreply.github.com>
2023-09-10 09:57:05 +0000

    Create desktop_predict.dart

---
+++ energia_dashboard/lib/screens/desktop/desktop_predict.dart
@@ -0,0 +1,60 @@
+import 'package:energia_dashboard/components/desktop/desktop_predict_content.dart';
+import 'package:energia_dashboard/components/desktop/desktop_sidebar.dart';
+import 'package:flutter/material.dart';
+
+class DesktopPredict extends StatelessWidget {
+  const DesktopPredict({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: Container(
+          margin: const EdgeInsets.only(left: 20),
+          child: Image.asset("assets/images/logo.jpg"),
+        ),
+        title: const Text(
+          'Energia AI',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        elevation: 1,
+        centerTitle: true,
+        actions: [
+          Container(
+            margin: const EdgeInsets.only(right: 10),
+            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+            height: 50,
+            decoration: const BoxDecoration(
+              borderRadius: BorderRadius.all(
+                Radius.circular(15),
+              ),
+              color: Colors.black,
+            ),
+            child: const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Text(
+                  "princesamuel@gmail.com",
+                  style: TextStyle(color: Colors.white),
+                ),
+                SizedBox(width: 5),
+                CircleAvatar(
+                  child: Icon(Icons.person),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+      body: const Padding(
+        padding: EdgeInsets.only(bottom: 10, left: 10, right: 10),
+        child: Row(
+          children: [
+            DesktopSidebar(predictActive: true),
+            DesktopPredictContent(),
+          ],
+        ),
+      ),
+    );
+  }
+}
