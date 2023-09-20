defeaa00e8435d5dc478cd9b6a59bf405b70edd3
DevPrinceK <80714449+DevPrinceK@users.noreply.github.com>
2023-09-20 12:32:31 +0000

    Update power_chart.dart

--- energia_dashboard/lib/charts/desktop/power_chart.dart
+++ energia_dashboard/lib/charts/desktop/power_chart.dart
@@ -1,5 +1,10 @@
+// ignore_for_file: use_build_context_synchronously
+
+import 'package:energia_dashboard/network/endpoints.dart';
 import 'package:flutter/material.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class PowerData {
   final String region;
@@ -13,59 +18,134 @@ class PowerData {
   );
 }
 
-class PowerChart extends StatelessWidget {
+class PowerChart extends StatefulWidget {
   final double heightFactor;
   PowerChart({super.key, this.heightFactor = 0.25});
-  final List<PowerData> data = [
-    PowerData('Ashanti', 5000, 5400),
-    PowerData('Greater Accra', 5100, 5400),
-    PowerData('Central', 6000, 4000),
-    PowerData('Northern', 5500, 5000),
-    PowerData('Western', 5000, 5400),
-    PowerData('Eastern', 5090, 3400),
-    PowerData('Volta', 5001, 4000),
-    PowerData('Brong-Ahafo', 3800, 5400),
-    PowerData('Upper East', 5000, 4500),
-    PowerData('Upper West', 5900, 2000),
-  ];
+
+  @override
+  State<PowerChart> createState() => _PowerChartState();
+}
+
+class _PowerChartState extends State<PowerChart> {
+  List<PowerData> RegionPowerData = [];
+  bool isLoading = false;
+
+  Future<int> fetchDashboardAnalysis() async {
+    // start loading
+    setState(() {
+      isLoading = true;
+    });
+
+    try {
+      // Make API call
+      var url = Uri.parse(APIEndpoints.powerByAllRegions);
+      var response = await http.get(url);
+
+      // stop loading
+      setState(() {
+        isLoading = false;
+      });
+
+      if (response.statusCode == 200) {
+        var resData = jsonDecode(response.body);
+        List<dynamic> powerDataList = resData.map((data) {
+          return PowerData(
+            data["region"] as String,
+            data["power_generation"] as double,
+            data["power_consumption"] as double,
+          );
+        }).toList();
+        print(powerDataList);
+        setState(() {
+          RegionPowerData = powerDataList.cast<PowerData>();
+        });
+        return response.statusCode;
+      } else {
+        print(response.statusCode);
+        print(response.reasonPhrase);
+        return response.statusCode;
+      }
+    } catch (e) {
+      print("And error occurred");
+      print(e);
+      // Handle exceptions by displaying a Snackbar
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text("An Error Occurred: $e"),
+        ),
+      );
+
+      // Stop loading
+      setState(() {
+        isLoading = false;
+      });
+
+      // Return an error code or handle it as needed
+      return -1; // You can return a custom error code or value
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    fetchDashboardAnalysis();
+  }
 
   @override
   Widget build(BuildContext context) {
     double deviceWidth = MediaQuery.of(context).size.width;
     return SizedBox(
-      height: deviceWidth * heightFactor,
-      child: Card(
-        child: SfCartesianChart(
-          primaryXAxis: CategoryAxis(
-            isVisible: true,
-            labelRotation: 45,
-            title: AxisTitle(text: "Regions"),
-          ),
-          primaryYAxis: NumericAxis(
-            isVisible: true,
-            title: AxisTitle(text: "Power"),
-          ),
-          series: <ChartSeries<PowerData, String>>[
-            StackedLineSeries<PowerData, String>(
-              dataSource: data,
-              xValueMapper: (PowerData power, _) => power.region,
-              yValueMapper: (PowerData power, _) => power.generation,
-              name: 'Power Generation',
+      height: deviceWidth * widget.heightFactor,
+      child: isLoading
+          ? const Center(
+              child: Stack(
+                alignment: Alignment.center,
+                children: [
+                  SizedBox(
+                    height: 100,
+                    width: 100,
+                    child: CircularProgressIndicator(),
+                  ),
+                  Center(
+                    child: Text(
+                      "Loading...",
+                    ),
+                  ),
+                ],
+              ),
+            )
+          : Card(
+              child: SfCartesianChart(
+                primaryXAxis: CategoryAxis(
+                  isVisible: true,
+                  labelRotation: 45,
+                  title: AxisTitle(text: "Regions"),
+                ),
+                primaryYAxis: NumericAxis(
+                  isVisible: true,
+                  title: AxisTitle(text: "Power"),
+                ),
+                series: <ChartSeries<PowerData, String>>[
+                  StackedLineSeries<PowerData, String>(
+                    dataSource: RegionPowerData,
+                    xValueMapper: (PowerData power, _) => power.region,
+                    yValueMapper: (PowerData power, _) => power.generation,
+                    name: 'Power Generation',
+                  ),
+                  StackedLineSeries<PowerData, String>(
+                    dataSource: RegionPowerData,
+                    xValueMapper: (PowerData power, _) => power.region,
+                    yValueMapper: (PowerData power, _) => power.consumption,
+                    name: 'Power Consumption',
+                  ),
+                ],
+                legend: const Legend(
+                  isResponsive: true,
+                  isVisible: true,
+                  position: LegendPosition.top,
+                ),
+              ),
             ),
-            StackedLineSeries<PowerData, String>(
-              dataSource: data,
-              xValueMapper: (PowerData power, _) => power.region,
-              yValueMapper: (PowerData power, _) => power.consumption,
-              name: 'Power Consumption',
-            ),
-          ],
-          legend: const Legend(
-            isResponsive: true,
-            isVisible: true,
-            position: LegendPosition.top,
-          ),
-        ),
-      ),
     );
   }
 }
