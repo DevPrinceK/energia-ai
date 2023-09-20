d4879dc91755e0e917cc324dcac217671d89b549
DevPrinceK <80714449+DevPrinceK@users.noreply.github.com>
2023-09-20 12:32:41 +0000

    Create endpoints.dart

---
+++ energia_dashboard/lib/network/endpoints.dart
@@ -0,0 +1,5 @@
+class APIEndpoints {
+  static const String baseUrl = "http://192.168.137.1:8000/"; //no vpn ip
+  static const String login = "${baseUrl}account/login/";
+  static const String powerByAllRegions = "${baseUrl}api/power-by-all-regions/";
+}
