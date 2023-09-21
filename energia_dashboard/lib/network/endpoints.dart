96c810c07e3b5df647e022d84f75c5650b5f6e3d
DevPrinceK <80714449+DevPrinceK@users.noreply.github.com>
2023-09-21 08:57:47 +0000

    Update endpoints.dart

--- energia_dashboard/lib/network/endpoints.dart
+++ energia_dashboard/lib/network/endpoints.dart
@@ -2,4 +2,5 @@ class APIEndpoints {
   static const String baseUrl = "http://192.168.137.1:8000/"; //no vpn ip
   static const String login = "${baseUrl}account/login/";
   static const String powerByAllRegions = "${baseUrl}api/power-by-all-regions/";
+  static const String predictPowerOutage = "${baseUrl}api/predict-outage/";
 }
